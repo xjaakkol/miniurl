e1adaad4c6da51e15d2189b710e791d706db8705
Jukka Jaakkola <ext-jukka.jaakkola@elisa.fi>
2023-05-10 09:54:45 +0300

    Add benchmark

--- miniurl_test.go
+++ miniurl_test.go
@@ -1,6 +1,7 @@
 package miniurl_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -25,4 +26,17 @@ func TestHashIsDeterministic(t *testing.T) {
 	output1 := miniurl.Hash(input)
 	output2 := miniurl.Hash(input)
 	assert.Equal(t, output1, output2)
+}
+
+func ExampleHash() {
+	const input = "https://github.com/xjaakkol/miniurl"
+	output := miniurl.Hash(input)
+	fmt.Println(output)
+}
+
+func BenchmarkHash(b *testing.B) {
+	const input = "https://github.com/xjaakkol/miniurl"
+	for n := 0; n < b.N; n++ {
+		miniurl.Hash(input)
+	}
 }
